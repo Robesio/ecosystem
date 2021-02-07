7f42d9646fb2c190d9ada15992369fa1b4dde691
LuccasTxr <luccas.teixeira@gmail.com>
2021-02-07 11:33:17 -0300

    crud tipos login

---
+++ missao-frotas/projeto/view/tiposlogin/tiposlogin.js
@@ -0,0 +1,86 @@
+const xhr = new XMLHttpRequest();
+const btn = document.getElementById("btn")
+const msg = document.getElementById("mensagem")
+const bp = document.getElementById("bp")
+
+var URLbase = "https://projetorrw.000webhostapp.com/src/controll/routes/"
+
+function readall() {
+    fetch(URLbase + "route.tipos_login.php?id=0")
+        .then(function (resp) {
+            if (!resp.ok)
+                throw new Error("Erro ao executar requisição: " + resp.status)
+            return resp.json()
+        })
+        .then(function (data) {
+            data.forEach((val) => {
+                let r = document.createElement("tr")
+                r.innerHTML = `<td>${val.id} </td>`
+                r.innerHTML += `<td>${val.tipo} </td>`
+                r.innerHTML += `<td style="padding:3px"><button onclick='edittipologin(this)'><i class="fa fa-pencil" aria-hidden="true"></i></button><button onclick='deltipologin(this)'><i class="fa fa-trash-o" aria-hidden="true"></i></button></td></tr>`
+
+                bp.appendChild(r)
+            })
+        })
+        .catch(function (error) {
+            console.error(error.message)
+        })
+}
+
+function addtipologin() {
+    let url = "https://projetorrw.000webhostapp.com/src/controll/routes/route.tipos_login.php"
+    let id = document.getElementById("id")
+    let tipo = document.getElementById("tipo")
+    if (id.value != "" && tipo.value != "") {
+        let dados = new FormData()
+        dados.append("id", id.value)
+        dados.append("tipo", tipo.value)
+        dados.append("verbo", "POST")
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+                let resp = JSON.parse(this.responseText);
+                msg.innerHTML = this.responseText;
+                if (resp.hasOwnProperty("erro")) {
+                    msg.innerHTML = resp.erro;
+                } else {
+                    msg.innerHTML = "Tipo de Login Criado Com Sucesso.";
+                }
+                setTimeout(() => { window.location.reload(); }, 3000);
+            }
+        })
+        xhr.open("POST", url);
+        xhr.send(dados);
+    } else {
+        msg.innerHTML = "Falta preencher todos os campos!";
+        setTimeout(() => { msg.innerHTML = "Mensagens do sistema"; }, 3000);
+    }
+}
+function edittipologin(v) {
+    v.parentNode.parentNode.cells[0].setAttribute("contentEditable", "true");
+    v.parentNode.parentNode.cells[1].setAttribute("contentEditable", "true");
+    v.parentNode.parentNode.cells[2].setAttribute("contentEditable", "true");
+    v.parentNode.parentNode.cells[3].innerHTML = "<button onclick='addtipologin(this)'>Enviar</button>";
+}
+function deltipologin(v) {
+    let url = "https://projetorrw.000webhostapp.com/src/controll/routes/route.tipos_login.php"
+    let id = v.parentNode.parentNode.cells[0].innerText
+    let dados = new FormData();
+    dados.append("id", id);
+    dados.append("verbo", "DELETE");
+    if (window.confirm("Confirma Exclusão do id = " + id + "?")) {
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === this.DONE) {
+                //console.log(this.responseText)
+                let resp = JSON.parse(this.responseText);
+                if (resp.hasOwnProperty("erro")) {
+                    msg.innerHTML = resp.erro
+                } else {
+                    msg.innerHTML = "Tipo de Login excluido Com Sucesso."
+                }
+                setTimeout(() => { window.location.reload(); }, 3000)
+            }
+        });
+        xhr.open("POST", url)
+        xhr.send(dados)
+    }
+}
