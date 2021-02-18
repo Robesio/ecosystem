227123589f6845c0bb78c69ec1f215fc58c0a12d
Robesio <robesiodocarmo@gmail.com>
2021-02-17 23:20:52 -0300

    testes

--- missao-rotas/projeto/ponto a ponto/ponto.js
+++ missao-rotas/projeto/ponto a ponto/ponto.js
@@ -29,13 +29,13 @@ function readall() {
 
 function addponto() {
     let url = "https://projetorrw.000webhostapp.com/src/controll/routes/route.pontos.php";
-    let id = document.getElementById("id_p");
-    let lati = document.getElementById("lati");
-    let longi = document.getElementById("longi");
-    if (id.value != "" && longi.value != "" && lati.value != "") {
+    let rotas_id = document.querySelector("#rotas_id");
+    let lat = document.querySelector("#lat");
+    let longi = document.querySelector("#longi");
+    if (rotas_id.value != "" && lat.value != "" && longi.value != "") {
         let dados = new FormData();
-        dados.append("rotas_id", id.value);
-        dados.append("lat", lati.value);
+        dados.append("rotas_id", rotas_id.value);
+        dados.append("lat", lat.value);
         dados.append("longi", longi.value);
         dados.append("verbo", "POST");
         xhr.addEventListener("readystatechange", function () {
